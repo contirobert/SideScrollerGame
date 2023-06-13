1b4913809821dd4cf7953f2945681c878a7bcc85
Robert Conti <32399797+contirobert@users.noreply.github.com>
2023-06-13 15:48:02 -0400

    Updated RunnerCharacter.cpp
    - Added component headers
    - Instantiated SideViewCamera
    - Defined controller params

--- Source/SideScrollerGame/RunnerCharacter.cpp
+++ Source/SideScrollerGame/RunnerCharacter.cpp
@@ -2,6 +2,9 @@
 
 
 #include "RunnerCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ARunnerCharacter::ARunnerCharacter()
@@ -9,6 +12,29 @@ ARunnerCharacter::ARunnerCharacter()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.f);
+	GetCapsuleComponent()->SetCollisionResponseToChannel
+	(ECC_GameTraceChannel1, ECR_Overlap);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	SideViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Side View Camera"));
+	SideViewCamera->bUsePawnControlRotation = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 720.0f, 0.0f);
+
+	GetCharacterMovement()->GravityScale = 2.0f;
+	GetCharacterMovement()->AirControl = 0.8f;
+	GetCharacterMovement()->JumpZVelocity = 1000.0f;
+	GetCharacterMovement()->GroundFriction = 3.0f;
+	GetCharacterMovement()->MaxWalkSpeed = 600.0f;
+	GetCharacterMovement()->MaxFlySpeed = 600.0f;
+
+	tempPos = GetActorLocation();
+	zPosition = tempPos.Z + 300.0f;
 }
 
 // Called when the game starts or when spawned
